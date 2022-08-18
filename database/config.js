00391d31377664c02a803e3bc9bab1d2b42a096a
Jeronimo-Junior <jeronimojunior.contato@outlook.com>
2022-08-17 22:32:52 -0300

    docker

--- database/config.js
+++ database/config.js
@@ -1,16 +1,26 @@
 const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//   host:"localhost",
+//   user: "root",
+//   port: 3306,
+//   // password: "2708200315tT@",
+//   database: 'CCI',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
 const pool = mysql.createPool({
-  host:"localhost",
-  user: "root",
-  port: 3306,
-  // password: "2708200315tT@",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
   database: 'CCI',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
 
 // pool.promise().query('select * from aluno')
 // .then( (result) => {
